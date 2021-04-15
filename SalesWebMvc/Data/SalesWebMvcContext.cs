7d9802cfe8fc229130ffa53f78a9ada168c19804
Rodrigo Barbosa <rodrigo_rhoads@hotmail.com>
2021-04-15 16:59:52 +0100

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -11,5 +11,8 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<Departament> Departament { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
